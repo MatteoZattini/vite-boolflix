9d0bfe79b61cc9823629bc2aa8485edb5b195686
MatteoZattini <matteozattini@hotmail.com>
2024-05-30 17:17:01 +0200

    m4 completa

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -33,30 +33,33 @@ export default {
             <div class="col-12">
                 <h1>FILMS</h1>
             </div>
-            <div class="col-12 d-flex flex-wrap overflow-auto">
-                <div v-for="movie in store.movies" class="movie-box text-center">
-                    <div>
-                        <img :src="'https://image.tmdb.org/t/p/original'+ movie.poster_path" alt="">
+            <div class="col-12 d-flex flex-wrap">
+                <div v-for="movie in store.movies" class="movie-box text-center position-relative">
+                    <div class="img-box">
+                        <img class="object-fit-cover" :src="'https://image.tmdb.org/t/p/original'+ movie.poster_path" alt="">
                     </div>
-                    <div>
-                        <h4>{{ movie.title }}</h4>
-                    </div>
-                    <div>
-                        <h5>{{ movie.original_title }}</h5>
-                    </div>
-                    <div class="d-flex justify-content-center">
-                        <div class="flag" :class="`fi, fi-`+ movie.original_language"></div>
-                    </div>
-                    <div>
-                        <span>{{ movie.original_language }}</span>
-                    </div>
-                    <div>
-                        <span v-if="(movie.vote_average / 2) >= 0.5"><i class="fa-solid fa-star"></i></span>
-                        <span v-if="(movie.vote_average / 2) >= 1.5"><i class="fa-solid fa-star"></i></span>
-                        <span v-if="(movie.vote_average / 2) >= 2.5"><i class="fa-solid fa-star"></i></span>
-                        <span v-if="(movie.vote_average / 2) >= 3.5"><i class="fa-solid fa-star"></i></span>
-                        <span v-if="(movie.vote_average / 2) >= 4.5"><i class="fa-solid fa-star"></i></span>
-                        <span class="ms-2">{{ (movie.vote_average / 2).toFixed(0) }}</span>
+                    <div class="hover-card position-absolute top-0 start-0">
+                        <div>
+                            <h4>{{ movie.title }}</h4>
+                        </div>
+                        <div>
+                            <h5>{{ movie.original_title }}</h5>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <div class="flag" :class="`fi, fi-`+ movie.original_language"></div>
+                        </div>
+                        <div>
+                            <span>{{ movie.original_language }}</span>
+                        </div>
+                        <div>
+                            <span v-if="(movie.vote_average / 2) >= 0.5"><i class="fa-solid fa-star"></i></span>
+                            <span v-if="(movie.vote_average / 2) >= 1.5"><i class="fa-solid fa-star"></i></span>
+                            <span v-if="(movie.vote_average / 2) >= 2.5"><i class="fa-solid fa-star"></i></span>
+                            <span v-if="(movie.vote_average / 2) >= 3.5"><i class="fa-solid fa-star"></i></span>
+                            <span v-if="(movie.vote_average / 2) >= 4.5"><i class="fa-solid fa-star"></i></span>
+                            <span class="ms-2">{{ (movie.vote_average / 2).toFixed(0) }}</span>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -65,16 +68,40 @@ export default {
 </template>
 
 <style scoped>
+.hover-card {
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    z-index: -1;
+}
+
+img:hover {
+    opacity: 0;
+}
+
+
+.img-box {
+    width: 100%;
+    height: 28rem;
+}
 
 .movie-box {
-    width: calc(100% / 5);
-    border: 1px solid white;
+    width: calc(100% / 4 - 1rem);
+    height: auto;
+    /* border: 1px solid white; */
     margin-bottom: 3rem;
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
 
 }
 
 img {
     width: 100%;
+    height: 100%;
+    object-position: top;
 }
 
 .flag {
--- src/components/AppTvSeries.vue
+++ src/components/AppTvSeries.vue
@@ -32,29 +32,32 @@ export default {
                 <h1>TV Series</h1>
             </div>
             <div class="col-12 d-flex flex-wrap">
-                <div v-for="show in store.series" class="movie-box text-center">
-                    <div>
-                        <img :src="'https://image.tmdb.org/t/p/original'+ show.poster_path" alt="">
+                <div v-for="show in store.series" class="movie-box text-center position-relative">
+                    <div class="img-box">
+                        <img class="object-fit-cover" :src="'https://image.tmdb.org/t/p/original'+ show.poster_path" alt="">
                     </div>
-                    <div>
-                        <h4>{{ show.name }}</h4>
-                    </div>
-                    <div>
-                        <h5>{{ show.original_name }}</h5>
-                    </div>
-                    <div class="d-flex justify-content-center">
-                        <div class="flag" :class="`fi, fi-`+ show.original_language"></div>
-                    </div>
-                    <div>
-                        <span>{{ show.original_language }}</span>
-                    </div>
-                    <div>
-                        <span v-if="(show.vote_average / 2) >= 0.5"><i class="fa-solid fa-star"></i></span>
-                        <span v-if="(show.vote_average / 2) >= 1.5"><i class="fa-solid fa-star"></i></span>
-                        <span v-if="(show.vote_average / 2) >= 2.5"><i class="fa-solid fa-star"></i></span>
-                        <span v-if="(show.vote_average / 2) >= 3.5"><i class="fa-solid fa-star"></i></span>
-                        <span v-if="(show.vote_average / 2) >= 4.5"><i class="fa-solid fa-star"></i></span>
-                        <span class="ms-2">{{ (show.vote_average / 2).toFixed(0) }}</span>
+                    <div class="hover-card position-absolute top-0 start-0">
+                        <div>
+                            <h4>{{ show.name }}</h4>
+                        </div>
+                        <div>
+                            <h5>{{ show.original_name }}</h5>
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <div class="flag" :class="`fi, fi-`+ show.original_language"></div>
+                        </div>
+                        <div>
+                            <span>{{ show.original_language }}</span>
+                        </div>
+                        <div>
+                            <span v-if="(show.vote_average / 2) >= 0.5"><i class="fa-solid fa-star"></i></span>
+                            <span v-if="(show.vote_average / 2) >= 1.5"><i class="fa-solid fa-star"></i></span>
+                            <span v-if="(show.vote_average / 2) >= 2.5"><i class="fa-solid fa-star"></i></span>
+                            <span v-if="(show.vote_average / 2) >= 3.5"><i class="fa-solid fa-star"></i></span>
+                            <span v-if="(show.vote_average / 2) >= 4.5"><i class="fa-solid fa-star"></i></span>
+                            <span class="ms-2">{{ (show.vote_average / 2).toFixed(0) }}</span>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -63,15 +66,38 @@ export default {
 </template>
 
 <style scoped>
+.hover-card {
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    z-index: -1;
+}
+
+img:hover {
+    opacity: 0;
+}
+
+.img-box {
+    width: 100%;
+    height: 28rem;
+}
+
 .movie-box {
-    width: calc(100% / 5);
-    border: 1px solid white;
+    width: calc(100% / 4 - 1rem);
+    /* border: 1px solid white; */
     margin-bottom: 3rem;
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
 
 }
 
 img {
     width: 100%;
+    height: 100%;
+    object-position: top;
 }
 
 .flag {
